062e9ab82a415f7b609bcae6c837c3a98c6e570d
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-06-30 12:25:59 +0200

    feat(lsp): add supports_method to lsp client

--- tests/lsp_spec.lua
+++ tests/lsp_spec.lua
@@ -93,4 +93,22 @@ describe("lsp client", function()
     assert.False(success)
     assert.Not.Nil(string.find(err, "Client 1 has shut down"))
   end)
+
+  a.it("returns a response for whether a method is supported", function()
+    local expected_result = false
+    local expected_method = "textDocument/diagnostic"
+    local expected_opts = { a = "b" }
+    vim.lsp.get_client_by_id = function(id)
+      return {
+        supports_method = function(method, opts)
+          assert.equals(expected_method, method)
+          assert.same(expected_opts, opts)
+          return expected_result
+        end,
+      }
+    end
+    local client = nio.lsp.get_client_by_id(1)
+    local result = client.supports_method.textDocument_diagnostic(expected_opts)
+    assert.equals(expected_result, result)
+  end)
 end)
